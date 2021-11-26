88ccfc5d054c194620c908e518b2e67aacec9d8b
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2021-11-27 00:02:41 +0900

    use environment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { RelayEnvironmentProvider } from "react-relay";
 
-function App() {
+import { relayEnvironment } from "./lib/relayEnvironment";
+import "./App.css";
+
+const App: React.VFC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RelayEnvironmentProvider environment={relayEnvironment}>
+      <React.Suspense fallback="Loading..." />
+    </RelayEnvironmentProvider>
   );
-}
+};
 
 export default App;
