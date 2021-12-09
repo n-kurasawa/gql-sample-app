adade38b2ff76c78505fa16f435ec70d8786e496
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2021-12-09 19:16:19 +0900

    add data

--- server/src/data/blog.ts
+++ server/src/data/blog.ts
@@ -16,6 +16,54 @@ const BLOGS: BlogData[] = [
     id: "3",
     name: "びっくりブログ",
   },
+  {
+    id: "4",
+    name: "つれづれ草子",
+  },
+  {
+    id: "5",
+    name: "インターネット観察日記",
+  },
+  {
+    id: "6",
+    name: "びっくりブログ",
+  },
+  {
+    id: "7",
+    name: "つれづれ草子",
+  },
+  {
+    id: "8",
+    name: "インターネット観察日記",
+  },
+  {
+    id: "9",
+    name: "びっくりブログ",
+  },
+  {
+    id: "10",
+    name: "つれづれ草子",
+  },
+  {
+    id: "11",
+    name: "インターネット観察日記",
+  },
+  {
+    id: "12",
+    name: "びっくりブログ",
+  },
+  {
+    id: "13",
+    name: "つれづれ草子",
+  },
+  {
+    id: "14",
+    name: "インターネット観察日記",
+  },
+  {
+    id: "15",
+    name: "びっくりブログ",
+  },
 ];
 
 export async function findBlog(id: string): Promise<BlogData | undefined> {
